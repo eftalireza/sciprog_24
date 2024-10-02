0461f1218ebcf8ba36f3237e9b22810a191fadf9
Alireza Eftekhari <eftalireza@proton.me>
2024-10-02 21:03:10 +0100

    Added Sum.f90 to Practical02

---
+++ Practical02/Sum.f90
@@ -0,0 +1,33 @@
+
+program sum
+! Declare variables
+   integer (kind=4) :: i, n
+   real (kind=4) :: sum1, sum2, diff
+   
+
+! First sum
+   sum1 = 0.0
+   do i=1,1000
+      
+	sum1 = sum1 + 1/real(i)
+
+   end do
+
+
+! Second sum 
+   sum2 = 0.0
+   do i=1000,1,-1
+   
+	sum2= sum2 + 1/real(i)
+
+   end do
+
+   write(6,*) ' Sum1=',sum1
+   write(6,*) ' Sum2=',sum2
+
+! Find the difference
+   diff = sum1 - sum2
+
+   write(6,*) ' Difference between the two is ',diff
+
+end program sum
